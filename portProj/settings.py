742695f6f0d2991f559e50ca215f6148574e5361
devPrasanthKumar <prok52010get@gmail.com>
2023-12-05 00:37:54 -0800

    new

--- portProj/settings.py
+++ portProj/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('skey')
-SECRET_KEY = "036bca0f4627cffacd4733f8488e0a58"
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
+SECRET_KEY = os.getenv("skey")
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
